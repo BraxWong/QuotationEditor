97f2a9edc2fda180f7195d8418e3cf97a5fe1d7c
Brax Wong <83001805+BraxWong@users.noreply.github.com>
2023-06-21 09:23:13 +0800

    Update buttonactions.cpp

--- CT_Pro/ChungTinUtil/buttonactions.cpp
+++ CT_Pro/ChungTinUtil/buttonactions.cpp
@@ -38,9 +38,18 @@ bool appendToFile(customerDetails* details, std::vector<entry*> entries)
 {
 
 
-    int total = 0;
+    int total = 0, totalDiscount = 0;
+    float totalMR = 0;
     Book* book = xlCreateBook();
    
+    Font* underlineBoldFont = book->addFont();
+    underlineBoldFont->setBold(true);
+    underlineBoldFont->setUnderline(UNDERLINE_SINGLE);
+    underlineBoldFont->setSize(10);
+    Format* underlineBoldFormat = book->addFormat();
+    underlineBoldFormat->setFont(underlineBoldFont);
+    underlineBoldFormat->setAlignH(ALIGNH_LEFT);
+
     Font* boldFont = book->addFont();
     boldFont->setBold();
     boldFont->setSize(18);
@@ -52,11 +61,27 @@ bool appendToFile(customerDetails* details, std::vector<entry*> entries)
 
     Font* bold2Font = book->addFont();
     bold2Font->setBold();
-    bold2Font->setSize(12);
+    bold2Font->setSize(10);
     bold2Font->setName(L"Microsoft YaHei UI");
     Format* bold2Format = book->addFormat();
     bold2Format->setFont(bold2Font);
     bold2Format->setAlignH(ALIGNH_CENTER);
+
+    Font* boldFontRight = book->addFont();
+    boldFontRight->setBold();
+    boldFontRight->setSize(10);
+    boldFontRight->setName(L"Microsoft YaHei UI");
+    Format* bold2FormatRight = book->addFormat();
+    bold2FormatRight->setFont(boldFontRight);
+    bold2FormatRight->setAlignH(ALIGNH_RIGHT);
+
+    Font* boldFontLeft = book->addFont();
+    boldFontLeft->setBold();
+    boldFontLeft->setSize(10);
+    boldFontLeft->setName(L"Microsoft YaHei UI");
+    Format* bold2FormatLeft = book->addFormat();
+    bold2FormatLeft->setFont(boldFontLeft);
+    bold2FormatLeft->setAlignH(ALIGNH_LEFT);
 
     Font* normalFont = book->addFont();
     normalFont->setSize(10);
@@ -90,29 +115,59 @@ bool appendToFile(customerDetails* details, std::vector<entry*> entries)
     Font* underlineFont = book->addFont();
     underlineFont->setUnderline(UNDERLINE_SINGLE);
     underlineFont->setBold(true);
-    underlineFont->setSize(12);
+    underlineFont->setSize(10);
 
     Format* underlineFormat = book->addFormat();
     underlineFormat->setFont(underlineFont);
     underlineFormat->setAlignH(ALIGNH_CENTER);
 
-
+    Font* cubicFont = book->addFont();
+    cubicFont->setSize(10);
+    cubicFont->setUnderline(UNDERLINE_SINGLEACC);
     Format* cubicFormat = book->addFormat();
-    cubicFormat->setBorderBottomColor(COLOR_BLACK);
-    cubicFormat->setBorderTopColor(COLOR_WHITE);
-    cubicFormat->setBorderRightColor(COLOR_WHITE);
-    cubicFormat->setBorderLeftColor(COLOR_WHITE);
-    cubicFormat->setFont(normalFont);
-    cubicFormat->setBorder(BORDERSTYLE_MEDIUM);
+    cubicFormat->setFont(cubicFont);
     cubicFormat->setAlignH(ALIGNH_CENTER);
     cubicFormat->setAlignV(ALIGNV_CENTER);
+
+    Format* dollarFormat = book->addFormat(normalFormatRight);
+    dollarFormat->setNumFormat(book->addCustomNumFormat(L"_( $* #,##0.00_);_( $* (#,##0.00);_( $* -??_);_(@_)"));
+
+    Font* discountFont = book->addFont();
+    discountFont->setUnderline(UNDERLINE_SINGLEACC);
+    Format* discountFormat = book->addFormat();
+    discountFormat->setNumFormat(book->addCustomNumFormat(L"_(-$* #,##0.00_);_(-$* (#,##0.00);_(-$* -??_);_(@_)"));
+    Format* finalDiscountFormat = book->addFormat(discountFormat);
+    finalDiscountFormat->setFont(discountFont);
+    finalDiscountFormat->setAlignH(ALIGNH_RIGHT);
+
+    
+
+    Format* MJFormat = book->addFormat(normalFormatRight);
+    MJFormat->setNumFormat(book->addCustomNumFormat(L"#.0#\"MJ/HR\"" ));
+
+    Font* MJBoldFont = book->addFont();
+    MJBoldFont->setBold(true);
+    MJBoldFont->setSize(10);
+    Format* MJBoldFormat = book->addFormat();
+    MJBoldFormat->setFont(MJBoldFont);
+    MJBoldFormat->setNumFormat(book->addCustomNumFormat(L"#.0#\"MJ/HR\""));
+
+    Font* dollarBoldFont = book->addFont();
+    dollarBoldFont->setBold(true);
+    dollarBoldFont->setSize(10);
+    dollarBoldFont->setUnderline(UNDERLINE_DOUBLE);
+    Format* dollarBoldFormat = book->addFormat(dollarFormat);
+    dollarBoldFormat->setFont(dollarBoldFont);
+    dollarBoldFormat->setAlignH(ALIGNH_RIGHT);
+
+
 
     Sheet* sheet = HELPFUNCTIONS_H::insertPicture(&book, "Logo.png", 0, 10);
     sheet->setName(L"中天單入紙");
 
     sheet->setCol(0, 10, 8);
     sheet->setRow(1,25);
-    sheet->setCol(1, 1, 30);
+    sheet->setCol(1, 1, 40);
     sheet->setCol(2, 2, 4);
     sheet->setCol(3, 5, 14);
     sheet->setRow(6, 15);
@@ -181,32 +236,39 @@ bool appendToFile(customerDetails* details, std::vector<entry*> entries)
     for (int i = 0; i < entries.size(); ++i)
     {
         int columnNum = 0;
-        sheet->writeNum(rowNum, columnNum, i, normalFormat);
+        sheet->writeNum(rowNum, columnNum, i + 1, normalFormat);
         columnNum++;
-        std::string stoveName = entries[i]->getProductName();
-        std::wstring stoveName2(stoveName.begin(), stoveName.end());
-        sheet->writeStr(rowNum, columnNum, stoveName2.c_str(), normalFormatLeft);
+        std::wstring stoveName = entries[i]->getProductName();
+        sheet->writeStr(rowNum, columnNum, stoveName.c_str(), normalFormatLeft);
         columnNum++;
         sheet->writeNum(rowNum, columnNum, entries[i]->getQuantity(), normalFormat);
         columnNum++;
-        std::string pricePerUnit = "$" + std::to_string(entries[i]->getPricePerUnit());
+        std::string pricePerUnit = std::to_string(entries[i]->getPricePerUnit());
         std::wstring pricePerUnit2(pricePerUnit.begin(), pricePerUnit.end());
-        sheet->writeStr(rowNum, columnNum, pricePerUnit2.c_str(), normalFormatRight);
+        sheet->writeNum(rowNum, columnNum, entries[i]->getPricePerUnit(), dollarFormat);
         columnNum++;
         int currentProductTotal = entries[i]->getQuantity() * entries[i]->getPricePerUnit();
         total += currentProductTotal;
-        std::string totalPrice = "$" + std::to_string(currentProductTotal);
-        std::wstring totalPrice2(totalPrice.begin(), totalPrice.end());
-        sheet->writeStr(rowNum, columnNum, totalPrice2.c_str(), normalFormatRight);
+        sheet->writeNum(rowNum, columnNum, currentProductTotal, dollarFormat);
         columnNum++;
-        std::string mj = std::to_string(entries[i]->getMJHR()) + "MJ/HR";
-        std::wstring mj2(mj.begin(), mj.end());
-        sheet->writeStr(rowNum, columnNum, mj2.c_str(), normalFormatRight);
+        sheet->writeNum(rowNum, columnNum, entries[i]->getMJHR(), MJFormat);
+        totalMR += entries[i]->getMJHR();
         rowNum++;
         columnNum = 1;
-        std::string model = entries[i]->getModel();
-        std::wstring model2(model.begin(), model.end());
-        sheet->writeStr(rowNum, columnNum, model2.c_str(), normalFormatLeft);
+        std::wstring model;
+        if (entries[i]->getAddOn() != L"N/A")
+        {
+            model += L"(" + entries[i]->getAddOn() + L")";
+        }
+        if (entries[i]->getModel() != L"N/A")
+        {
+            model += L"型號:" + entries[i]->getModel();
+        }
+        if (entries[i]->getApprovalNumber() != L"N/A")
+        {
+            model += L",批核編號(" + entries[i]->getApprovalNumber() + L")";
+        }
+        sheet->writeStr(rowNum, columnNum, model.c_str(), normalFormatLeft);
         rowNum++;
         std::string dimensions = entries[i]->getDimensionsX() + "X" + entries[i]->getDimensionsY() + "X" + entries[i]->getDimensionsZ() + "H";
         std::wstring dimensions2(dimensions.begin(), dimensions.end());
@@ -219,16 +281,48 @@ bool appendToFile(customerDetails* details, std::vector<entry*> entries)
             sheet->writeNum(rowNum, columnNum, entries[i]->getDiscount(), normalFormat);
             columnNum++;
             int discount = entries[i]->getDiscount() * entries[i]->getPricePerUnit();
-            std::string discountString = "-$" + std::to_string(discount);
-            std::wstring discountString2(discountString.begin(), discountString.end());
-            sheet->writeStr(rowNum, columnNum, discountString2.c_str(), normalFormatRight);
+            sheet->writeNum(rowNum, columnNum, discount, discountFormat);
             columnNum++;
-            sheet->writeStr(rowNum, columnNum, discountString2.c_str(), normalFormatRight);
-            total -= discount;
+            sheet->writeNum(rowNum, columnNum, discount, discountFormat);
+            totalDiscount += discount;
         }
         rowNum += 2;
     }
-
+    sheet->writeStr(rowNum, 0, L"*", bold2FormatRight);
+    sheet->writeStr(rowNum, 1, L"以上報價不包括煤氣錶按金及街喉費用", bold2FormatLeft);
+    rowNum++;
+    sheet->writeStr(rowNum, 4, L" ", cubicFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 3, L"合共金額:", normalFormatRight);
+    sheet->writeNum(rowNum, 4, total, dollarFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 3, L"香港中華煤氣有限公司『尊貴客戶』優惠 - 合共金額:", normalFormatRight);
+    sheet->writeNum(rowNum, 4, totalDiscount, finalDiscountFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 3, L"客人應付金額:", bold2FormatRight);
+    sheet->writeNum(rowNum, 4, total - totalDiscount, dollarBoldFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 3, L"合共兆焦耳(MJ/HR)", bold2FormatRight);
+    sheet->writeNum(rowNum, 4, totalMR, MJBoldFormat);
+    rowNum += 2;
+    sheet->writeStr(rowNum, 0, L"* 備注 : ", bold2FormatRight);
+    sheet->writeStr(rowNum, 1, L"1)上述 (尊貴客戶) 爐具優惠以香港中華煤氣有限公司最後批核作準。", underlineBoldFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 1, L"2)上述兆焦耳(MJ/HR) 以最後現場已完成之爐具批核作準, 如超過1150MJ/HR, ", underlineBoldFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 1, L"客人需自行申報環保。", underlineBoldFormat);
+    rowNum++;
+    sheet->writeStr(rowNum, 1, L"上述所有由香港中華煤氣有限公司提供(尊貴客戶)優惠",normalFormatLeft);
+    rowNum++;
+    sheet->writeStr(rowNum, 1, L"之爐具已包括本公司首年之免費維修保養及零件更換.", normalFormatLeft);
+    rowNum += 2;
+    sheet->setMerge(rowNum, rowNum, 4, 5);
+    sheet->writeStr(rowNum, 1, L"客戶簽署及蓋印", normalFormatLeft);
+    sheet->writeStr(rowNum, 4, L"中天廚房設備有限公司", normalFormat);
+    rowNum += 5;
+    sheet->setMerge(rowNum, rowNum, 4, 5);
+    sheet->writeStr(rowNum, 1, L" ", cubicFormat);
+    sheet->writeStr(rowNum, 4, L" ", cubicFormat);
     
 
     book->save(wstr.c_str());
