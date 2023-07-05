7a4fb0d3dc0656fcf85a27ef5cc8ffe8e0f331a6
Brax Wong <83001805+BraxWong@users.noreply.github.com>
2023-07-05 19:01:20 +0800

    Update Database.h

--- CT_Pro/ChungTinUtil/Database.h
+++ CT_Pro/ChungTinUtil/Database.h
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
+#include <vector>
 class Database
 {
 	public:
 		void databaseEntry(std::wstring ao, std::wstring pName, std::wstring modelName, std::string pDimensionsX, std::string pDimensionsY, 
 			std::string pDimensionsZ, std::wstring approveNum, int quant, int price, float MH);
 		bool itemExistsInDatabase(std::wstring ao);
+		std::vector<std::wstring> dataToEditor(std::wstring ao);
 };
 
