3d2a08db8853489a19c6fc1dee1d3eed216024aa
Brax Wong <ghostenjob@gmail.com>
2024-03-19 20:33:12 +0000

    feature: Completed writing header of the excel file,

--- QuotationEditorPython/src/XlsxWriter.py
+++ QuotationEditorPython/src/XlsxWriter.py
@@ -8,5 +8,34 @@ class XlsxWriter:
     def writeToXlsx(self):
         workbook = xlsxwriter.Workbook(self.orderDetails.fileName)
         worksheet = workbook.add_worksheet()
-        worksheet.write('A1', 'hello World')
+        merge_format = self.createFormat(workbook, 22, '標楷體', True)
+        merge_format2 = self.createFormat(workbook, 14, 'Times New Roman', True) 
+        merge_format3 = self.createFormat(workbook, 12, '標楷體', False)
+        url_format = workbook.add_format({'font_color': 'blue',
+                                          'underline': True,
+                                          'align': 'center'
+                                          })
+        url_format.set_align('vcenter')
+        worksheet.set_column('B:B', 50)
+        worksheet.set_column('C:C', 4)
+        worksheet.set_row(0, 30)
+        worksheet.merge_range('A1:F1', '中天廚房設備有限公司', merge_format)
+        worksheet.merge_range('A2:F2', 'CHUNG TIN KITCHEN WARES COMPANY LIMITED', merge_format2)
+        worksheet.merge_range('A3:F3', '香港九龍馬頭角道105號地下', merge_format3)
+        worksheet.merge_range('A4:F4', 'G/F., 105 Ma Tau Kok Road, Kowloon, Hong Kong', merge_format3)
+        worksheet.merge_range('A5:F5', '電話: (852) 2363 1482  傳真: (852) 2766 1415', merge_format3)
+        worksheet.merge_range('A6:F6', '機電處註冊氣體工程承辦商號碼 RGC NO.-(682-06)', merge_format3)
+        worksheet.merge_range('A7:F7', '', url_format)
+        worksheet.write_url('A7', 'chungtinkitchen@netvigator.com', url_format, 'E-mail: chungtinkitchen@netvigator.com')
         workbook.close()
+
+    def createFormat(self, workbook: xlsxwriter.Workbook, font_size, font_name, bold):
+        
+        merge_format = workbook.add_format({'align': 'center',
+                                            'bold': bold,
+                                          })
+        merge_format.set_align('vcenter')
+        merge_format.set_font_size(font_size)
+        merge_format.set_font_name(font_name)
+        return merge_format
+        
--- QuotationEditorPython/src/main.py
+++ QuotationEditorPython/src/main.py
@@ -1,12 +1,19 @@
 from PySide6 import QtWidgets
 import sys
 import MainMenu
+import XlsxWriter
+import Entry
+import OrderDetails
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MainMenu.MainMenu()
-    widget.resize(800, 600)
-    widget.show()
-
-    sys.exit(app.exec())
+    entry = Entry.Entry("addOn", "product1", "model1", "12", "13", "14", "approveNum", "12", "130000", 13.21, 1)
+    order = OrderDetails.OrderDetails("Brax", "Flat C", "Nicole", "1234", "Nobody", "testing.xlsx", 91001426, "TX-1000")
+    xlsxWriter = XlsxWriter.XlsxWriter(entry, order)
+    xlsxWriter.writeToXlsx()
+    # app = QtWidgets.QApplication([])
+    #
+    # widget = MainMenu.MainMenu()
+    # widget.resize(800, 600)
+    # widget.show()
+    #
+    # sys.exit(app.exec())
